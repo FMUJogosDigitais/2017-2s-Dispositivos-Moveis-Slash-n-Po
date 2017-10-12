66d01584cb1aa8e4822ea8b84863104839df66b8
MaxterPlays <raphael.s.amadeu@gmail.com>
2017-10-12 15:04:32 -0300

    second version of the game

--- slashNpo/Assets/Scripts/mainController.cs
+++ slashNpo/Assets/Scripts/mainController.cs
@@ -7,12 +7,17 @@ public class mainController : MonoBehaviour {
 
 	public character player;
 	public character enemy;
+	public enemyAI enemy_AI;
 	public Image player_energy;
 	public Image enemy_energy;
 	public Text timer;
-	public playerUI ui;	
+	public Image ballon;
+	public GameObject rock;
+	public GameObject paper;
+	public GameObject scisor;
 
 	int game_status=1;
+	float time = 60f;
 
 	//TODO: musica e efeitos sonoros
 
@@ -24,10 +29,43 @@ public class mainController : MonoBehaviour {
 	}	
 	
 	void Update () {
+		processaTempo();
 		
 	}
 
 	public int getGameStatus(){
 		return game_status;
+	}
+
+	public void playerAttack(int a){		
+		player.attack(a);
+		int e = enemy_AI.getCurrentAttack();		
+		enemy.attack(e);
+	}
+
+	public void pauseGame(){
+		Debug.Log("Pause");
+	}
+
+	public void setBallon(int b){
+		rock.SetActive(false);
+		paper.SetActive(false);
+		scisor.SetActive(false);
+		if(b==1)
+			rock.SetActive(true);
+		else if(b==2)
+			paper.SetActive(true);
+		else if(b==3)
+			scisor.SetActive(true);
+	}
+
+
+	void processaTempo(){
+		time -= Time.deltaTime*1;
+		float t = Mathf.Floor(time);		
+		if(t<0)
+			t=0;
+		timer.text = ""+t;
+
 	}
 }
